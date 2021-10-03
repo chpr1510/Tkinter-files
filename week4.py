fea3410674c7662c3c7c4c779a38fb19a37d796d
prashanth chinni <54094195+chpr1510@users.noreply.github.com>
2021-10-03 14:37:36 +0530

    Add files via upload

---
+++ week4.py
@@ -0,0 +1,68 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 78,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import tkinter\n",
+    "window=tkinter.Tk()\n",
+    "\n",
+    "window.title(\"hotel room registration\")\n",
+    "\n",
+    "title=tkinter.Label(window,text=\"Tile\").pack()\n",
+    "fn=tkinter.Label(window,text=\"First name\").pack()\n",
+    "ln=tkinter.Label(window,text=\"Last name\").pack()\n",
+    "sw=tkinter.Label(window,text=\"Share with\").pack()\n",
+    "bn=tkinter.Label(window,text=\"Business number\").pack()\n",
+    "mn=tkinter.Label(window,text=\"Mobile number\").pack()\n",
+    "email=tkinter.Label(window,text=\"Email address\").pack()\n",
+    "doa=tkinter.Label(window,text=\"Date of arrival\").pack()\n",
+    "dod=tkinter.Label(window,text=\"Date of departure\").pack()\n",
+    "name=tkinter.Label(window,text=\"Name on credit card\").pack()\n",
+    "cn=tkinter.Label(window,text=\"Credit card number\").pack()\n",
+    "date=tkinter.Label(window,text=\"Expiry date\").pack()\n",
+    "cvv=tkinter.Label(window,text=\"CVV number\").pack()\n",
+    "mode=tkinter.Label(window,text=\"Payment method\").pack()\n",
+    "\n",
+    "window.mainloop()"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
